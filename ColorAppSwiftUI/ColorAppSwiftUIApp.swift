069b8535bbb49a23e22b275b2f025bb347047aeb
Alexander <alex.pismenskiy@gmail.com>
2020-12-15 20:27:12 +0300

    Initial Commit

---
+++ ColorAppSwiftUI/ColorAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorAppSwiftUIApp.swift
+//  ColorAppSwiftUI
+//
+//  Created by Alexander Pismenskiy on 15.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct ColorAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
